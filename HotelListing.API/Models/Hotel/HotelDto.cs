863fe266019143b098810087c4b8403b8c52d54e
Chevol Davis <chevol.davis@gmail.com>
2023-01-17 00:16:52 +0700

    Added hotel repository and hotel controller

--- HotelListing.API/Models/Hotel/HotelDto.cs
+++ HotelListing.API/Models/Hotel/HotelDto.cs
@@ -1,12 +1,6 @@
 ﻿namespace HotelListing.API.Models.Hotel;
 
-public class HotelDto
+public class HotelDto : BaseHotelDto
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Address { get; set; }
-    public Double Rating { get; set; }
-
-    public int CountryId { get; set; }
 }
-
