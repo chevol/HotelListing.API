73bc8017e6120359a2a6bf3f3c569a5cc1b8d499
chevol.davis <chevol.davis@gmail.com>
2023-03-20 18:01:12 -0400

    Added Paging

--- HotelListing.API/Controllers/CountriesController.cs
+++ HotelListing.API/Controllers/CountriesController.cs
@@ -2,6 +2,7 @@
 using HotelListing.API.Contracts;
 using HotelListing.API.Data;
 using HotelListing.API.Exceptions;
+using HotelListing.API.Models;
 using HotelListing.API.Models.Country;
 using HotelListing.API.Repository;
 using Microsoft.AspNetCore.Authorization;
@@ -28,13 +29,21 @@ namespace HotelListing.API.Controllers
             this._logger = logger;
         }
 
-        // GET: api/Countries
-        [HttpGet]
+        // GET: api/Countries/GetAll
+        [HttpGet("GetAll")]
         public async Task<ActionResult<IEnumerable<GetCountryDto>>> GetCountries()
         {
             var countries = await _countriesRepository.GetAllAsync();
             var records = _mapper.Map<List<GetCountryDto>>(countries);
             return Ok(records);
+        }
+
+        // GET: api/Countries/?StartIndex=0&pageSize=25&PageNumber=1
+        [HttpGet]
+        public async Task<ActionResult<PagedResult<GetCountryDto>>> GetPagedCountries([FromQuery] QueryParameters queryParameters)
+        {
+            var pagedConuntriesResuilt = await _countriesRepository.GetAllAsync<GetCountryDto>(queryParameters);
+            return Ok(pagedConuntriesResuilt);
         }
 
         // GET: api/Countries/5
